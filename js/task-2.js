83011ad5431a19ada55c8264cf99cc8bc879c7f5
Aleksan Vartapetyan <alex.240941.123@gmail.com>
2024-01-24 21:31:57 +0900

    update js files

--- js/task-2.js
+++ js/task-2.js
@@ -11,7 +11,11 @@ class Storage {
   }
   removeItem(itemToRemove) {
     const searchElement = this.#items.indexOf(itemToRemove);
-    this.#items.splice(searchElement, 1);
+    if (searchElement !== -1) {
+      this.#items.splice(searchElement, 1);
+      return `${itemToRemove} removed successfully.`;
+    }
+    return console.log("This element is not found");
   }
 }
 
@@ -21,29 +25,3 @@ storage.addItem("Droid");
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
 storage.removeItem("Prolonger");
 console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
-
-// I decided to implement two different methods, 'getItems,' because '#items' is a private property. Below, I demonstrate this using a getter
-
-// class Storage {
-//   #items;
-//   constructor(items) {
-//     this.#items = items;
-//   }
-//   get items() {
-//     return this.#items;
-//   }
-//   addItem(newItem) {
-//     this.#items.push(newItem);
-//   }
-//   removeItem(itemToRemove) {
-//     const searchElement = this.#items.indexOf(itemToRemove);
-//     this.#items.splice(searchElement, 1);
-//   }
-// }
-
-// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
-// console.log(storage.items); // ["Nanitoids", "Prolonger", "Antigravitator"]
-// storage.addItem("Droid");
-// console.log(storage.items); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
-// storage.removeItem("Prolonger");
-// console.log(storage.items); // ["Nanitoids", "Antigravitator", "Droid"]
--- js/task-3.js
+++ js/task-3.js
@@ -1,24 +1,24 @@
 class StringBuilder {
   #value;
   constructor(inititalValue) {
-    this.#value = [inititalValue];
+    this.#value = inititalValue;
   }
 
   getValue() {
-    return this.#value.join("");
+    return this.#value;
   }
 
   padEnd(str) {
-    this.#value.push(str);
+    const lastElement = this.#value.length - 1;
+    this.#value += str;
   }
 
   padStart(str) {
-    this.#value.unshift(str);
+    this.#value = str + this.#value;
   }
 
   padBoth(str) {
-    this.#value.push(str);
-    this.#value.unshift(str);
+    this.#value = str + this.#value + str;
   }
 }
 const builder = new StringBuilder(".");
@@ -29,37 +29,3 @@ builder.padEnd("^");
 console.log(builder.getValue()); // "^.^"
 builder.padBoth("=");
 console.log(builder.getValue()); // "=^.^="
-
-// I decided to make an implementation similar to the previous one.
-
-// class StringBuilder {
-//   #value;
-//   constructor(inititalValue) {
-//     this.#value = [inititalValue];
-//   }
-
-//   get value() {
-//     return this.#value.join("");
-//   }
-
-//   padEnd(str) {
-//     this.#value.push(str);
-//   }
-
-//   padStart(str) {
-//     this.#value.unshift(str);
-//   }
-
-//   padBoth(str) {
-//     this.#value.push(str);
-//     this.#value.unshift(str);
-//   }
-// }
-// const builder = new StringBuilder(".");
-// console.log(builder.value); // "."
-// builder.padStart("^");
-// console.log(builder.value); // "^."
-// builder.padEnd("^");
-// console.log(builder.value); // "^.^"
-// builder.padBoth("=");
-// console.log(builder.value); // "=^.^="
